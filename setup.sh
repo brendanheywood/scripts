b7f5978601eacfc2fb0b6094e88096f85ce113c8
Brendan Heywood <brendan@catalyst-au.net>
2014-08-12 09:47:29 +1000

    Added .vim setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # Install .bashrc
-
 if grep 'scripts/.bashrc' ~/.bashrc
 then
   echo '[ok] Bashrc'
@@ -9,6 +8,20 @@ else
   echo 'Setting up .bashrc'
   echo '. ~/scripts/.bashrc' >> ~/.bashrc
   . ~/.bashrc
+fi
+
+# Sym link .vim dir
+if [ -L ~/.vim ]; then
+  # It is a symlink!
+  # Symbolic link specific commands go here.
+  echo "[ok] .vim $LINK_OR_DIR"
+elif [ -d ~/.vim ]; then 
+  # It's a directory!
+  # Directory command goes here.
+  echo "[bad] .vim dir exists!"
+else
+  ln -s ~/scripts/.vim ~/.vim
+  echo "[ok] Created ~/.vim/ sym link"
 fi
 
 
