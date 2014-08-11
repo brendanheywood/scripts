0b01a123ae71bebe729d9249e8a0cab373848b7e
Brendan Heywood <brendan@catalyst-au.net>
2014-08-12 09:52:26 +1000

    Added .vimrc setup

--- setup.sh
+++ setup.sh
@@ -24,4 +24,18 @@ else
   echo "[ok] Created ~/.vim/ sym link"
 fi
 
+# Sym link .vimrc
+if [ -L ~/.vimrc ]; then
+  # It is a symlink!
+  # Symbolic link specific commands go here.
+  echo "[ok] .vimrc $LINK_OR_DIR"
+elif [ -e ~/.vimrc ]; then 
+  # It's a directory!
+  # Directory command goes here.
+  echo "[bad] .vimrc exists!"
+else
+  ln -s ~/scripts/.vimrc ~/.vimrc
+  echo "[ok] Created ~/.vimrc sym link"
+fi
+
 
